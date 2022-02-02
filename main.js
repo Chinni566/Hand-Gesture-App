b3f47375998906096a633204ecb6ef904c6fd177
Neeraj <78613839+Chinni566@users.noreply.github.com>
2022-02-02 18:27:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ Webcam.set({
     image_format: 'png',
     png_quality: 90
     });
+    
     camera - document.getElementById("camera");
     Webcam.attach( '#camera' );
     
@@ -17,6 +18,52 @@ Webcam.set({
     
     classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xwh8XHkwR/model.json', modelLoaded);
     
-    function modelLoaded(){
-        console.log('Model Loaded!');
+    function modelLoaded() {
+        console.log("Model Loaded");
     }
+    
+    function speak() {
+        var synth = window.speechSynthesis;
+        speak_data = "The meaning of this emoji is  " + Prediction;
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis)
+    }
+    
+    function check() {
+        img = document.getElementById("captured_image");
+        classifier.classify(img, gotResults);
+    }
+    
+    function gotResults(error, results) {
+        if(error) {
+            console.error(error);
+        }
+    
+        else {
+            console.log(results);
+            document.getElementById("result_emoji_meaning").innerHTML = results[0].label;
+            Prediction = results[0].label;
+            speak();
+    
+            if(results[0].label=="Amazing") {
+                document.getElementById("update_emoji").innerHTML = "&#128076";
+            }
+            
+            if(results[0].label=="Good") {
+                document.getElementById("update_emoji").innerHTML = "&#128077";
+            }
+            
+            if(results[0].label=="Victory") {
+                document.getElementById("update_emoji").innerHTML = "&#9996";
+            }
+
+            if(results[0].label=="Anger") {
+                document.getElementById("update_emoji").innerHTML = "&#9994";
+            }
+
+            if(results[0].label=="Hello") {
+                document.getElementById("update_emoji").innerHTML = "&#128079";
+            }
+        }
+    }
+    
